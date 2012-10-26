778c5cfb9c11a5c484b4098f5d6238ab092c8c0b
Dipesh Batheja <dipesh_batheja@yahoo.com>
2012-10-26 21:31:38 +0530

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tasqify::Application.config.secret_token = '5d92a692114d4ddc2c62f195df32edb8a021667e940f7234c3f62912e4cbb7561dc48052f9c317678ed08de7d6aeb0d5e686b95e58dfd7a116c8f8aa45649e79'
